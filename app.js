97f7cd133bfdc41718085b46844c48015148ee78
SritharS <sritharsdckap@gmail.com>
2023-03-21 12:27:33 +0530

    fa

--- app.js
+++ app.js
@@ -12,15 +12,46 @@ let Space = document.getElementById("Space")
 // console.log(Space);
 let Shift = document.getElementById("Shift")
 // console.log(Shift);
-let binsspan = document.querySelectorAll("#bispan")
-console.log(binsspan);
+// let binsspan = document.qublockerySelectorAll("#bispan")
+// console.log(binsspan);
 let spans = document.querySelectorAll("span")
 // console.log(spans);
+let fistdiv = document.querySelectorAll(".divtwo")
+// console.log(fistdiv);
+let tamildiv = document.getElementById("tamildiv")
+// console.log(tamildiv);
+let tamil = document.getElementById("tamil")
+// console.log(tamil);
+let eng = document.getElementById("eng")
+
+let tamiltext = document.querySelector("#tamiltext")
+// console.log(tamiltext);
+
+let specialchar = document.querySelector('.specialchar')
+let divone = document.querySelector(".divone")
+// console.log(divone);
+// console.log(english);
+let main =document.querySelector(".main")
+// console.log(main);
+let main1 =document.querySelector(".main1")
+
+let tamilbtn = document.querySelectorAll(".tamilbtn")
+// console.log(tamilbtn);
+
+// window.addEventListener("keyup",(e)=>{
+//     // console.log(e.key);
+
+//     result.append(e.key)
+// })
+window.addEventListener("keyup", function (e) {
+    console.log(e.key);
+})
+
 
 for (let i = 0; i < btns.length; i++) {
     // console.log(btns[i]);
     btns[i].addEventListener("click",(e)=>{
-        if (btns[i].innerHTML !== "Backspoace" 
+        if (btns[i].innerHTML !== "Backspace" 
         && btns[i].innerHTML !== "Capslock" 
         && btns[i].innerHTML !== "Space"
         && btns[i].innerHTML !== "Ctrl"
@@ -28,17 +59,17 @@ for (let i = 0; i < btns.length; i++) {
         && btns[i].innerHTML !== "Alt"
         && btns[i].innerHTML !== "Shift"
         && btns[i].innerHTML !== "AltGr"
-        && btns[i].innerHTML !== "PrtSc")
+        && btns[i].innerHTML !== "PrtSc"
+        && btns[i].innerHTML !== "தமிழ்"
+        && btns[i].innerHTML !== "eng")
          {
             result.value += btns[i].innerText 
         }
         // console.log(e.target);
-        
-        
     })
 
     Capslock.addEventListener("click",()=>{
-        if(btns[i].innerHTML !== "Backspoace" && btns[i].innerHTML !== "AltGr" 
+        if(btns[i].innerHTML !== "Backspace" && btns[i].innerHTML !== "AltGr" 
          && btns[i].innerHTML !== "PrtSc"
          && btns[i].innerHTML !== "Ctrl" 
          && btns[i].innerHTML !== "Shift" 
@@ -50,7 +81,6 @@ for (let i = 0; i < btns.length; i++) {
             btns[i].classList.toggle('upper')
         }
     })
-    
 }
 
 Backspoace.addEventListener("click",()=>{
@@ -59,7 +89,7 @@ Backspoace.addEventListener("click",()=>{
 })
 
 Enter.addEventListener("click", ()=>{
-    result.value = "";
+    result.value = "\n";
 })
 
 Space.addEventListener("click", ()=>{
@@ -67,3 +97,43 @@ Space.addEventListener("click", ()=>{
     result.value +=` `; 
 })
 
+Shift.addEventListener("click",(e)=>{
+    // alert ("hii")
+        specialchar.classList.toggle('show')
+        divone.classList.toggle('none')
+})
+
+
+tamil.addEventListener("click",()=>{
+    // alert("ki")
+    main.style.display ="none"
+    main1.style.display="block"
+})
+eng.addEventListener("click",()=>{
+    main.style.display="block"
+    main1.style.display="none"
+})
+
+
+for(let j =0; j<tamilbtn.length;j++){
+    // console.log(tamilbtn[j]);
+    tamilbtn[j].addEventListener("click",(e)=>{
+        // console.log(e.target);
+        if(tamilbtn[j].innerHTML !== "eng"){
+            tamiltext.value += tamilbtn[j].innerText
+        }
+        
+    })
+}
+
+let tamilBackspoace = document.getElementById("tamilBackspoace")
+tamilBackspoace.addEventListener("click",()=>{
+    // console.log(e.target);
+    tamiltext.value = tamiltext.value.slice(0, -1);
+})
+
+let tamilEnter = document.getElementById("tamilEnter")
+
+tamilEnter.addEventListener("click", ()=>{
+    tamiltext.value = "\n";
+})
